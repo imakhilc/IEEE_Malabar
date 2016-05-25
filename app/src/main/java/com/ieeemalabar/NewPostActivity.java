f01eb91d413a320203807fcb84bfc700e139bcd3
imAKHILc <imakhilc@gmail.com>
2016-05-25 17:53:23 +0530

    Image uploading!

--- app/src/main/java/com/ieeemalabar/NewPostActivity.java
+++ app/src/main/java/com/ieeemalabar/NewPostActivity.java
@@ -1,30 +1,41 @@
 package com.ieeemalabar;
 
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.ieeemalabar.models.User;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.ieeemalabar.models.Post;
+import com.ieeemalabar.models.User;
 
-import java.text.DateFormat;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,11 +43,20 @@ public class NewPostActivity extends BaseActivity {
 
     private static final String TAG = "NewPostActivity";
     private static final String REQUIRED = "Required";
+    ProgressDialog pd;
 
     private DatabaseReference mDatabase;
+    private FirebaseStorage mStorage;
+    private StorageReference mStorageRef;
 
     private EditText mTitleField;
     private EditText mBodyField;
+    private ImageView post_img;
+    StorageReference imageRef;
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    final int PIC_CROP = 2;
+    Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +66,19 @@ public class NewPostActivity extends BaseActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getSupportActionBar().setElevation(0);
         }
-        
+
+        pd = new ProgressDialog(this);
+        pd.setCancelable(false);
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mStorage = FirebaseStorage.getInstance();
+        mStorageRef = mStorage.getReferenceFromUrl("gs://project-3576505284407387518.appspot.com/");
+
+        imageRef = mStorageRef.child("featured/default.jpg");
 
         mTitleField = (EditText) findViewById(R.id.field_title);
         mBodyField = (EditText) findViewById(R.id.field_body);
+        post_img = (ImageView) findViewById(R.id.post_img);
 
         findViewById(R.id.fab_submit_post).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,15 +87,21 @@ public class NewPostActivity extends BaseActivity {
             }
         });
 
-        //TextView pic_name = (TextView) findViewById(R.id.pic_name);
-        //pic_name.setText(formattedDate);
+        Button select = (Button) findViewById(R.id.img_buttom);
+        assert select != null;
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                //Toast.makeText(NewPostActivity.this, images.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void submitPost() {
         final String title = mTitleField.getText().toString().trim();
         final String body = mBodyField.getText().toString().trim();
-        TextView pic_name = (TextView) findViewById(R.id.pic_name);
-        Button img_buttom = (Button) findViewById(R.id.img_buttom);
 
         // Title is required
         if (TextUtils.isEmpty(title)) {
@@ -80,6 +114,7 @@ public class NewPostActivity extends BaseActivity {
             mBodyField.setError(REQUIRED);
             return;
         }
+
         // [START single_value_read]
         final String userId = getUid();
         mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
@@ -97,6 +132,10 @@ public class NewPostActivity extends BaseActivity {
                                     "Error: could not fetch user.",
                                     Toast.LENGTH_SHORT).show();
                         } else {
+
+                            pd.setMessage("Saving Report...");
+                            pd.show();
+
                             // Write new post
                             Calendar c = Calendar.getInstance();
                             System.out.println("Current time => " + c.getTime());
@@ -104,6 +143,11 @@ public class NewPostActivity extends BaseActivity {
                             SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
                             String date = df.format(c.getTime());
                             writeNewPost(userId, user.name, title, body, date);
+
+                            // Finish this Activity, back to the stream
+                            //startActivity(new Intent(getApplicationContext(), MainContainer.class));
+                            pd.hide();
+                            finish();
                         }
                     }
 
@@ -129,14 +173,99 @@ public class NewPostActivity extends BaseActivity {
 
         mDatabase.updateChildren(childUpdates);
 
-        // Finish this Activity, back to the stream
-        //startActivity(new Intent(getApplicationContext(), MainContainer.class));
-        finish();
+        imageRef = mStorageRef.child("featured/"+ key +".jpg");
+        post_img.setDrawingCacheEnabled(true);
+        post_img.buildDrawingCache();
+        Bitmap bitmap = post_img.getDrawingCache();
+        bitmap = getResizedBitmap(bitmap,550);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = imageRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        });
     }
+
     // [END write_fan_out]
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
         startActivity(new Intent(getApplicationContext(), MainContainer.class));
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            selectedImage = data.getData();
+            post_img.setImageURI(selectedImage);
+            post_img.setVisibility(View.VISIBLE);
+            TextView pic_name = (TextView) findViewById(R.id.pic_name);
+            pic_name.setText(selectedImage.toString());
+            //performCrop(selectedImage);
+        }
+        /*if (requestCode == PIC_CROP && data != null) {
+            // get the returned data
+            Bundle extras = data.getExtras();
+            // get the cropped bitmap
+            Bitmap selectedBitmap = extras.getParcelable("data");
+
+            ImageView post_img = (ImageView) findViewById(R.id.post_img);
+            post_img.setImageBitmap(selectedBitmap);
+            post_img.setVisibility(View.VISIBLE);
+        }*/
+    }
+
+    /*private void performCrop(Uri picUri) {
+        try {
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            // indicate image type and Uri
+            cropIntent.setDataAndType(picUri, "image/*");
+            // set crop properties
+            cropIntent.putExtra("crop", "true");
+            // indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            // indicate output X and Y
+            cropIntent.putExtra("outputX", 1000);
+            cropIntent.putExtra("outputY", 1000);
+            // retrieve data on return
+            cropIntent.putExtra("return-data", true);
+            // start the activity - we handle returning in onActivityResult
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        // respond to users whose devices do not support the crop action
+        catch (ActivityNotFoundException anfe) {
+            // display an error message
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }*/
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image, width, height, true);
     }
 }
